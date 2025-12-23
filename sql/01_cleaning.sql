760492b1eb266cfef843340cb127315b698250f6
Nirmal Ramakrishnan <nirmalrk@hotmail.com>
2025-12-23 10:52:21 +0100

    Add secondary  cleaning SQL

--- sql/01_cleaning.sql
+++ sql/01_cleaning.sql
@@ -8,3 +8,12 @@ SELECT
   ,string_field_5 AS aroma_category
   string_field_6 AS grape_type
 FROM `curious-mender-475710-a7.by_the_glass.wine_attributes_enriched` ;
+
+#cleaning mapping TABLE
+CREATE OR REPLACE TABLE `curious-mender-475710-a7.by_the_glass.bar_mapped` AS
+
+SELECT
+  string_field_0 AS bar
+  , string_field_1 AS Company
+  , string_field_2 AS bar_location
+FROM `curious-mender-475710-a7.by_the_glass.mapping_bar`
