5ff72b5bbc9765af6b10e1c09c7e68d74de58b3e
Nirmal Ramakrishnan <nirmalrk@hotmail.com>
2025-12-23 02:47:33 +0100

    Add initial BigQuery cleaning SQL

---
+++ sql/01_cleaning.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `curious-mender-475710-a7.by_the_glass.attributes_enriched`  AS
+SELECT
+  string_field_0 AS wine_name
+  ,string_field_1 AS color
+  ,tring_field_2 AS grape
+  ,string_field_3 AS residual_sugar
+  ,string_field_4 AS country
+  ,string_field_5 AS aroma_category
+  string_field_6 AS grape_type
+FROM `curious-mender-475710-a7.by_the_glass.wine_attributes_enriched` ;
